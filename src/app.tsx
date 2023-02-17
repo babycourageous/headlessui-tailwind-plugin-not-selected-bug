76be7898e4dcfa3d1089375f0fcaa4dee1d03516
Rene Dellefont <renedellefont@gmail.com>
2023-02-17 11:21:28 -0500

    basic example from docs

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
+import Example from "./example";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <Example />
   )
 }
